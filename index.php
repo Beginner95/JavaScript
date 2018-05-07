386fcbc011ddce7eaa8aa75de3d539f06d2814a2
Vaharsolta <vaha-22@mail.ru>
2018-05-07 17:02:41 +0300

    Я - JavaScript!

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Привет мир!</title>
+</head>
+<body>
+	<p>Начало документа...</p>
+	<script>
+		alert( 'Я - JavaScript!' );
+	</script>
+</body>
+</html>
