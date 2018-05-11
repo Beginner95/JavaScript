a2b3c5b3ef6b96d247e6f9bc7d3a593f918d7c14
Vaharsolta <vaha-22@mail.ru>
2018-05-11 17:26:36 +0300

    Написал функцию которая возвращает х в степени n

--- js/function-basics.js
+++ js/function-basics.js
@@ -1,5 +1,40 @@
-function checkAge(age) {
+/*function checkAge(age) {
     return (age > 18) || confirm('Родители разрешили?');
 }
 
 checkAge(19);
+
+function min(a, b) {
+    return (a < b) ? a : b;
+}
+alert(min(3, -1));
+*/
+
+let x = prompt('Введите число ', '');
+let n = prompt('Введите степерь ', '');
+
+if (n <= 1) {
+    alert('Степень не может быть меньще 1 и дробным числом');
+} else {
+    alert( pow(x, n));
+}
+
+function pow(x, n) {  
+  
+    let sum = x;
+    
+    for (let i = 1; i < n; i++) {
+        sum *= x;
+    }
+    
+    return sum;
+}
+
+
+
+
+
+
+
+
+
