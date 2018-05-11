797b202543188936b7307b54c2a8b400690d2164
Vaharsolta <vaha-22@mail.ru>
2018-05-11 10:23:07 +0300

    Заменил for на while

--- js/while-for.js
+++ js/while-for.js
@@ -2,5 +2,12 @@
 
 for (let i = 2; i <= 10; i++){
     if (i % 2 != 0) continue;
-    alert( i );
+    //alert( i );
+}
+
+let j = 0;
+
+while (j < 3) {
+    alert( 'номер ' + j + '!');
+    j++;
 }
