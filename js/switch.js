ab371bff4e59c9acc42ddce6dccad5f4fc0e0f81
Vaharsolta <vaha-22@mail.ru>
2018-05-11 15:31:47 +0300

    Решил задачу заменил if на switch

--- js/switch.js
+++ js/switch.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let browser = 'IE';
+/*let browser = 'IE';
 
 if (browser == 'IE') {
     alert('О, да у вас IE!');
@@ -8,4 +8,24 @@ if (browser == 'IE') {
     alert('Да, и эти браузере мы поддерживаем');
 } else {
     alert('Мы надеемся, что и в вашем браузере все ок!');
+}*/
+
+let a = prompt('a ?', '');
+
+switch (a) {
+    case '0':
+    alert(0);
+    break;
+    
+    case '1':
+    alert(1);
+    break;
+    
+    case '2':
+    case '3':
+    alert('2, 3');
+    break;
+    
+    default:
+    alert('Ни одно условие не подошло');
 }
