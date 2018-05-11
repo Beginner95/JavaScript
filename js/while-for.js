04f5abd43368da4dccfc7f685c5f3f3366b0f3ea
Vaharsolta <vaha-22@mail.ru>
2018-05-11 14:43:26 +0300

    Вывод простых чисел

--- js/while-for.js
+++ js/while-for.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 for (let i = 2; i <= 10; i++){
     if (i % 2 != 0) continue;
     //alert( i );
@@ -16,5 +16,22 @@ let num = 0;
 
 while (num < 100 && num != null) {
     num = prompt('Введите число больше 100', '');
-}
+}*/
+
+let a = 2;
+let n = 10;
+
+numbers:
+  for (let i = a; i <= n; i++) {
+
+    for (let j = a; j < i; j++) {
+      if (i % j == 0) continue numbers;
+    }
+
+    alert( i );
+  }
+
+
+
+
 
