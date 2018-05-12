7fd16b6a27d056a973d4ac9815b13657ee83be00
Vaharsolta <vaha-22@mail.ru>
2018-05-12 15:11:13 +0300

    Сделал pow по спецификации

--- index.php
+++ index.php
@@ -3,10 +3,25 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Привет мир!</title>
+    <script src="https://learn.javascript.ru/test/libs.js"></script>
+    <script src="/js/testing.js"></script>
 </head>
 <body>
-	<p>Начало документа...</p>
 
-	<script src="/js/recursion.js"></script>
+    <script>
+    
+        function pow(x, n) {
+            if (n < 0) return NaN;
+            if (Math.round(n) != n) return NaN;
+            
+            let sum = 1;
+            for (let i = 0; i < n; i++) {
+                sum *= x;
+            }
+            return sum;
+        }
+    
+    </script>
+	
 </body>
 </html>
