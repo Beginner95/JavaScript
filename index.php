6317c8402d2abe393082c2fe8b70b8db982b5576
Vaharsolta <vaha-22@mail.ru>
2018-05-13 09:21:57 +0300

    Решил задачу Интерфейс суммы

--- index.php
+++ index.php
@@ -3,25 +3,9 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Привет мир!</title>
-    <script src="https://learn.javascript.ru/test/libs.js"></script>
-    <script src="/js/testing.js"></script>
+    <script src="/js/number.js"></script>
 </head>
 <body>
-
-    <script>
-    
-        function pow(x, n) {
-            if (n < 0) return NaN;
-            if (Math.round(n) != n) return NaN;
-            
-            let sum = 1;
-            for (let i = 0; i < n; i++) {
-                sum *= x;
-            }
-            return sum;
-        }
-    
-    </script>
 	
 </body>
 </html>
---
+++ js/number.js
@@ -0,0 +1,6 @@
+'use strict';
+
+let a = +prompt('Введите первое число', '');
+let b = +prompt('Введите второе число', '');
+
+alert(a + b);
