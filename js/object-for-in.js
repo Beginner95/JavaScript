088c544d43293564683bd5b0d8f54f4835f70fde
Vaharsolta <vaha-22@mail.ru>
2018-05-15 20:30:16 +0300

    Решил задачу Свойство с наибольшим значением

--- js/object-for-in.js
+++ js/object-for-in.js
@@ -2,14 +2,22 @@
 
 let salaries = {
     'Vasy': 10,
-    'Pety': 3,
+    'Pety': 30,
     'Hope': 6
 }
 
 let sum = 0;
+let name = '';
 
 for (let key in salaries) {
-    sum += salaries[key];
+    
+    if (sum < salaries[key]) {
+        
+        sum = salaries[key];
+        name = key;
+        
+    }
+    
 }
 
-alert(sum);
+alert( name || 'Нет сотрудников' );
