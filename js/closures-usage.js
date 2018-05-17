ba94c0a9d1398c517001f0d722554a2d5ff3753b
Vaharsolta <vaha-22@mail.ru>
2018-05-17 11:21:19 +0300

    Решил задачу. Строковой буфер с очисткой

--- js/closures-usage.js
+++ js/closures-usage.js
@@ -3,17 +3,24 @@
 let makeBuffer = function() {
     let txt = '';
     
-    return function(str) {
+    let buffer = function(str) {
         if (arguments.length == 0) {
             return txt;
         }
         txt += str;
+    };
+    
+    buffer.clear = function() {
+        txt = '';
     }
-}
+    return buffer;
+};
 
 let buffer = makeBuffer();
 buffer(0.5);
 buffer('test');
 buffer(5);
+console.log(buffer());
+buffer.clear();
 console.log(buffer());
 
