972ba71069bf747d355d0b4d54cc06074bf3d6bb
Vaharsolta <vaha-22@mail.ru>
2018-05-20 20:44:51 +0300

    Решил задачу. Создайте калькулятор

--- js/constructor-new.js
+++ js/constructor-new.js
@@ -1,14 +1,44 @@
 'use strict';
 
-let Accumulator = function(startingValue) {
-    this.value = startingValue;
+let Calculator = function() {
+    let method = {
+        '-': function(a, b) {
+            return a - b;
+        },
+        '+': function(a, b) {
+            return a + b;
+        }
+    };
     
-    this.read = function() {
-        this.value += +prompt('Значение для добавления', '');
+    this.calculate = function(str) {
+        let split = str.split(' '),
+            a = +split[0],
+            op = split[1],
+            b = +split[2];
+            
+        if (!method[op] || isNaN(a) || isNaN(b)) {
+            return NaN;
+        }
+        
+        return method[op](a, b);
+    }
+    
+    this.addMethod = function(name, func) {
+        method[name] = func;
     };
 };
 
-let accumulator = new Accumulator(1);
-accumulator.read();
-accumulator.read();
-alert( accumulator.value );
+let calc = new Calculator;
+
+calc.addMethod('*', function(a, b) {
+  return a * b;
+});
+calc.addMethod('/', function(a, b) {
+  return a / b;
+});
+calc.addMethod('**', function(a, b) {
+  return Math.pow(a, b);
+});
+
+let result = calc.calculate('4 / 3');
+alert( result );
