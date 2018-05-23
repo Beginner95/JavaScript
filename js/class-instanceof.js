ab103a4c40236d595e73f8cd301a6a08a2166d44
Vaharsolta <vaha-22@mail.ru>
2018-05-23 17:54:18 +0300

    Решил задачу. Eval-калькулятор

--- js/class-instanceof.js
+++ js/class-instanceof.js
@@ -1,16 +1,6 @@
 'use strict';
 
-let printNumbersInterval = function() {
-    let i = 1;
-    let timerId = setInterval(function() {
-        console.log(i);
-        
-        if (i == 20) {
-            clearInterval(timerId);
-        }
-        i++;
-    }, 100);
-};
+let calc = prompt('Введите выражение?', '2*3+2');
 
-printNumbersInterval();
+alert( eval(calc) );
 
