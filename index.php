64d579e652fe3cc657262b287aacd7197ea3b28d
Vaharsolta <vaha-22@mail.ru>
2018-06-23 11:44:16 +0300

    Решиз дадание. DOM children

--- index.php
+++ index.php
@@ -5,6 +5,16 @@
 	<title>Привет мир!</title>    
 </head>
 <body>
-	<script src="/js/promise.js"></script>
+
+    <div>Пользователи:</div>
+    <ul>
+        <li>Masha</li>
+        <li>Vova</li>
+    </ul>
+    
+    <!-- comments -->
+    
+	<script src="/js/traversing-dom.js"></script>
+    
 </body>
 </html>
---
+++ js/traversing-dom.js
@@ -0,0 +1,9 @@
+'use strict';
+
+console.log( document.head );
+console.log( document.documentElement.children[0] );
+console.log( document.documentElement.firstChild );
+
+console.log( document.querySelector('ul') );
+console.log( document.body.children[1] );
+console.log( document.body.children[1].children[1] );
