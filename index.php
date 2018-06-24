f6833e73e3fdc927b3948d8dd34c5831c9f477d4
Vaharsolta <vaha-22@mail.ru>
2018-06-24 19:51:00 +0300

    Решил задачу. Поиск элементов

--- index.php
+++ index.php
@@ -2,19 +2,49 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Привет мир!</title>    
+	<title>Поиск: getElement* и querySelector* и не только</title>    
 </head>
 <body>
 
-    <div>Пользователи:</div>
-    <ul>
-        <li>Masha</li>
-        <li>Vova</li>
-    </ul>
+    <form name="search">
+    <label>Поиск по сайту:
+      <input type="text" name="search">
+    </label>
+    <input type="submit" value="Искать!">
+  </form>
+
+  <hr>
+
+  <form name="search-person">
+    Поиск по посетителям:
+    <table id="age-table">
+      <tr>
+        <td>Возраст:</td>
+        <td id="age-list">
+          <label>
+            <input type="radio" name="age" value="young">до 18</label>
+          <label>
+            <input type="radio" name="age" value="mature">18-50</label>
+          <label>
+            <input type="radio" name="age" value="senior">более 50</label>
+        </td>
+      </tr>
+
+      <tr>
+        <td>Дополнительно:</td>
+        <td>
+          <input type="text" name="info[0]">
+          <input type="text" name="info[1]">
+          <input type="text" name="info[2]">
+        </td>
+      </tr>
+
+    </table>
+
+    <input type="submit" value="Искать!">
+  </form>
     
-    <!-- comments -->
-    
-	<script src="/js/traversing-dom.js"></script>
+	<script src="/js/searching-elements-dom.js"></script>
     
 </body>
 </html>
---
+++ js/searching-elements-dom.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let lab = document.getElementById('age-table').getElementsByTagName('label');
+let res = document.querySelector('#age-table td');
+let form = document.getElementsByTagName('form')[1];
+let search = document.querySelector('[name="search"]');
+let inp = document.querySelector('[name="search"] input');
+let inf1 = document.getElementsByName("info[0]")[0];
+let inf2 = document.querySelector('[name="search-person"] [name="info[0]"]');
+
+console.log(lab);
+console.log(res);
+console.log(form);
+console.log(search);
+console.log(inp);
+console.log(inf1);
+console.log(inf2);
