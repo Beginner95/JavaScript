9905d0843ce057e09b556a38f50f8ddec7ca6083
Vaharsolta <vaha-22@mail.ru>
2018-07-01 18:23:46 +0300

    Решил задачу. Вставьте элементы в конец списка

--- index.php
+++ index.php
@@ -6,7 +6,10 @@
     <link rel="stylesheet" href="/style.css">
 </head>
 <body>
-
-<script src="/js/modifying-document.js"></script>
+<ul>
+    <li>1</li>
+    <li>2</li>
+</ul>
+<script src="/js/multi-insert.js"></script>
 </body>
 </html>
---
+++ js/multi-insert.js
@@ -0,0 +1,5 @@
+'use strict';
+
+let ul = document.body.children[0];
+
+ul.insertAdjacentHTML("beforeEnd", "<li>3</li><li>4</li><li>5</li>");
