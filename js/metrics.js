900576907c1871fb362f7a76000e1c1efecf3864
Vaharsolta <vaha-22@mail.ru>
2018-07-03 10:50:40 +0300

    Решил задачу. Узнать ширину полосы прокрутки

--- js/metrics.js
+++ js/metrics.js
@@ -1,5 +1,19 @@
 'use strict';
 
-let elem = document.getElementById('example');
+let elem = document.createElement('div');
 
-console.log(elem.scrollHeight - elem.scrollTop - elem.clientHeight);
+elem.style.overflowY = 'scroll';
+elem.style.height = '100px';
+elem.style.width = '100px';
+elem.style.visibility = 'hidden';
+
+document.body.appendChild(elem);
+
+console.log(elem.offsetWidth);
+console.log(elem.clientWidth);
+
+let scrollWidth = elem.offsetWidth - elem.clientWidth;
+
+document.body.removeChild(elem)
+
+console.log(scrollWidth);
