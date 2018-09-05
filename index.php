01d6fc91ee95a0b6e794a3bed4cf4d544e3d370b
Vaharsolta <vaha-22@mail.ru>
2018-09-05 15:39:41 +0300

    Решил задачу. Голосовалка

--- index.php
+++ index.php
@@ -7,22 +7,13 @@
     
 </head>
 <body>
-<pre>
-Клик на элементе выделяет только его.
-Ctrl(Cmd)+Клик добавляет/убирает элемент из выделенных.
-Shift+Клик добавляет промежуток от последнего кликнутого к выделению.
-</pre>
-
-<ul>
-    <li>Кристофер Робин</li>
-    <li>Винни-Пух</li>
-    <li>Ослик Иа</li>
-    <li>Мудрая Сова</li>
-    <li>Кролик. Просто кролик.</li>
-</ul>
+<div id="voter" class="voter">
+    <span class="down">—</span>
+    <span class="vote">0</span>
+    <span class="up">+</span>
+</div>
 
 <script src="/js/widgets-structure.js"></script>
-<button onclick="alert(listSelect.getSelected())">Показать отмеченные</button>
 
 </body>
 </html>
--- js/widgets-structure.js
+++ js/widgets-structure.js
@@ -1,74 +1,36 @@
 'use strict';
 
-let listSelect = new ListSelect({
-    elem: document.querySelector('ul')
-});
-
-function ListSelect(options) {
+function Voter(options) {
     let elem = options.elem;
-    let lastClickedLi = null;
+    let voteElem = elem.querySelector('.vote');
+    
+    elem.onclick = function(event) {
+        if (event.target.closest('.down')) {
+            voteDecrease();
+        } else if (event.target.closest('.up')) {
+            voteIncrease();
+        }
+    }
     
     elem.onmousedown = function() {
         return false;
     };
     
-    elem.onclick = function(e) {
-        let li = e.target.closest('li');
-        
-        if (!li) {
-            return;
-        }
-        
-        if (e.metaKey || e.ctrlKey) {
-            toggleSelect(li);
-        } else if (e.shiftKey) {
-            selectFromLast(li);
-        } else {
-            selectSingle(li);
-        }
-        
-        lastClickedLi = li;
-    };
-    
-    function deselectAll() {
-        [].forEach.call(elem.children, function(child) {
-            child.classList.remove('selected');
-        });
+    function voteDecrease() {
+        voteElem.innerHTML = +voteElem.innerHTML - 1;
     }
     
-    function toggleSelect(li) {
-        li.classList.toggle('selected');
+    function voteIncrease() {
+        voteElem.innerHTML = +voteElem.innerHTML + 1;
     }
     
-    function selectSingle(li) {
-        deselectAll();
-        li.classList.add('selected');
-    }
-    
-    function selectFromLast(target) {
-        let startElem = lastClickedLi || elem.children[0];
-        
-        target.classList.add('selected');
-        if (startElem == target) {
-            return;
-        }
-        
-        let isLastClickedBefore = startElem.compareDocumentPosition(target) & 4;
-        
-        if (isLastClickedBefore) {
-            for (let elem = startElem; elem != target; elem = elem.nextElementSibling) {
-                elem.classList.add('selected');
-            }
-        } else {
-            for (let elem = startElem; elem != target; elem = elem.previousElementSibling) {
-                elem.classList.add('selected');
-            }
-        }
-    }
-    
-    this.getSelected = function() {
-        return [].map.call(elem.querySelectorAll('.selected'), function(li) {
-            return li.innerHTML;
-        });
+    this.setVote = function(vote) {
+        voteElem.innerHTML = +vote;
     };
 }
+
+let voter = new Voter({
+    elem: document.getElementById('voter')
+});
+
+voter.setVote(1);
