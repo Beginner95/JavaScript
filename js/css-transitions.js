328aa608c306bf1110aa088ec3d25bed09057fa1
Vaharsolta <vaha-22@mail.ru>
2018-09-15 16:41:31 +0300

    Решил задачу. Анимирования картинки в формате Безье

--- js/css-transitions.js
+++ js/css-transitions.js
@@ -1,15 +1,6 @@
 'use strict';
 
 flyjet.onclick = function() {
-    let ended = false;
-
-    flyjet.addEventListener('transitionend', function() {
-        if (!ended) {
-            ended = true;
-            cl('Готово!');
-        }
-    });
-
     flyjet.classList.add('growing');
 }
 
