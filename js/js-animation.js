b9f562ea8dd2a3f009a81c1e8eb0876cc4afca3d
Vaharsolta <vaha-22@mail.ru>
2018-09-17 21:28:47 +0300

    Решил задачу Анимирование мяча с отскоком

--- js/js-animation.js
+++ js/js-animation.js
@@ -7,22 +7,35 @@ function makeEaseOut(timing) {
 }
 
 function bounce(timeFraction) {
-    for (let a = 0, b = 1, result; 1; a += b, b /= 2) {
+    for (var a = 0, b = 1, result; 1; a += b, b /= 2) {
         if (timeFraction >= (7 - 4 * a) / 11) {
             return -Math.pow((11 - 6 * a - 11 * timeFraction) / 4, 2) + Math.pow(b, 2)
         }
     }
 }
 
+function quad(timeFraction) {
+    return Math.pow(timeFraction, 2);
+}
+
 ball.onclick = function() {
 
-    let to = field.clientHeight - ball.clientHeight;
+let height = field.clientHeight - ball.clientHeight;
+let width = 100;
 
     animate({
-        duration: 3000,
+        duration: 2000,
         timing: makeEaseOut(bounce),
         draw: function(progress) {
-            ball.style.top = to * progress + 'px'
+            ball.style.top = height * progress + 'px'
         }
     });
-};
+
+    animate({
+        duration: 2000,
+        timing: makeEaseOut(quad),
+        draw: function(progress) {
+            ball.style.left = width * progress + "px"
+        }
+    });
+}
