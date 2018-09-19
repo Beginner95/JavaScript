b01b9487257a376c3d9a1501816a0c4a44005be7
Vaharsolta <vaha-22@mail.ru>
2018-09-19 19:06:49 +0300

    Нарисовал две окружности на canvas

--- js/canvas.js
+++ js/canvas.js
@@ -4,11 +4,19 @@ let canvas = _('canvas');
 let ctx = canvas.getContext('2d');
 let pi = Math.PI;
 
+ctx.beginPath();
 ctx.lineWidth = 5;
 ctx.strokeStyle = 'red';
 ctx.fillStyle = 'green';
+ctx.arc(80, 60, 50, 0, 2*pi, false);
+ctx.stroke();
+ctx.fill();
 
-ctx.arc(150, 60, 50, 0, 2*pi, false);
+ctx.beginPath();
+ctx.lineWidth = 5;
+ctx.strokeStyle = 'green';
+ctx.fillStyle = 'red';
+ctx.arc(180, 60, 40, 0, 2*pi, false);
 ctx.stroke();
 ctx.fill();
 
