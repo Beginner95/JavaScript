3359d3832a40959340f5917b6a4899a63e1a27bc
Vaharsolta <vaha-22@mail.ru>
2018-09-23 21:08:03 +0300

    Рисуем линию с нажатой кнопкой мышки

--- js/canvas.js
+++ js/canvas.js
@@ -3,16 +3,27 @@
 let canvas = _('canvas');
 let ctx = canvas.getContext('2d');
 let pi = Math.PI;
+let isMoseDown = false;
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-canvas.addEventListener('mousedown', function(event) {
+canvas.addEventListener('mousemove', function(event) {
     //c(event);
-    ctx.beginPath();
-    ctx.arc(event.clientX, event.clientY, 30, 0, pi * 2);
-    ctx.fill();
-    
+    //c(isMoseDown);
+    if (true === isMoseDown) {
+        ctx.beginPath();
+        ctx.arc(event.clientX, event.clientY, 30, 0, pi * 2);
+        ctx.fill();
+    }
+});
+
+canvas.addEventListener('mousedown', function(){
+    isMoseDown = true;
+});
+
+canvas.addEventListener('mouseup', function(){
+    isMoseDown = false;
 });
 
 
